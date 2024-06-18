53edef4d3a9a4cc5d6657b3633b8d1661912dd77
Husnain Izhar <husnainizhar@outlook.com>
2024-06-18 10:27:23 +0000

    latest

---
+++ zoom/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Link } from 'lucide-react'
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <header className='shadow'>
+        <div className='mx-auto flex h-14 max-w-5l items-center justify-between'>
+            <Link href='/'><div>New Meeting</div></Link>
+            <SignedIn>
+                <div className='flex items-center gap-5'>
+                    <Link href='/meetings'>Meetings</Link>
+                    <UserButton/>
+                </div>
+            </SignedIn>
+            <SignedOut><SignInButton/></SignedOut>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
